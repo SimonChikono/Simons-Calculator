70ff2ba8b66796662439eaedf149d35feea7c333
SimonChikono <schikono7@gmail.com>
2022-05-12 18:09:48 +0200

    tree 4

--- Simons-Calculator.php
+++ Simons-Calculator.php
@@ -4,3 +4,13 @@ function tree(string $text): void
     echo $text;
 }
 tree("param3t3r");
+
+function repeatContent(int $count = 1 ): void
+{
+    for($i = 0; $i <= $count; $i++){
+        sayHello("Poznan");
+    }
+}
+
+repeatContent();
+repeatContent(5);
